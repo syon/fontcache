1a9f92a9580077a1bcafe8f539bd28f418ccddf4
syon <syon.xiii@gmail.com>
2018-03-31 13:15:05 +0900

    :tada: it works

--- handler.js
+++ handler.js
@@ -1,29 +1,31 @@
 "use strict";
 const AWS = require("aws-sdk");
+const shell = require("shelljs");
+const fs = require("fs");
+const dg = require("debug")("main");
+const s3 = new AWS.S3();
+const Bucket = "syon-fontcache";
+const tmpCacheDir = "/tmp/cache/fontconfig";
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
-  const s3 = new AWS.S3();
-  const params = {
-    Bucket: "syon-fontcache",
-    Key: "example2.txt",
-    Body: "Uploaded text using the promise-based method!"
-  };
-  s3
-    .putObject(params)
-    .promise()
-    .then(data => {
-      console.log("Success");
-      callback(null, response);
+module.exports.main = (event, context, callback) => {
+  process.env.HOME = process.env.LAMBDA_TASK_ROOT;
+  shell.mkdir("-p", tmpCacheDir);
+  shell.exec(`fc-cache -v ${process.env.HOME}/.fonts`);
+  const promises = [];
+  shell.ls(tmpCacheDir).forEach((Key, i) => {
+    dg(`[${i}]`, `${tmpCacheDir}/${Key}`);
+    fs.readFile(`${tmpCacheDir}/${Key}`, (err, Body) => {
+      if (err) throw err;
+      const params = { Bucket, Key, Body };
+      promises.push(s3.putObject(params).promise());
+    });
+  });
+  Promise.all(promises)
+    .then(() => {
+      callback(null, { statusCode: 200 });
     })
     .catch(err => {
-      console.log(err);
-      callback(null, response);
+      dg(err);
+      callback(null, { statusCode: 500 });
     });
 };
